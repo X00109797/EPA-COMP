aefb3011caec721a7239fde58d19f180e877873b
epa <epa@localhost.localdomain>
2017-09-29 11:00:01 +0100

    CPU Count Updated 2

--- week3/script.sh
+++ week3/script.sh
@@ -6,7 +6,7 @@ echo "CPU Count"
 cnt=`grep process /proc/cpuinfo | wc -l`
 #cat /proc/cpuinfo | grep processor | wc -l
 
-if[$cnt -le 2];then
+if[ $cnt -le 2 ];then
 #action
 echo "Too Few CPUS, exiting"
 fi
